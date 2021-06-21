8be8a7188bb2fb0026b527fe551709d693d76158
henskumari <hennaojala99@gmail.com>
2021-06-21 14:12:59 +0300

    tehtävä 3.22

--- mongo.js
+++ mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -15,19 +15,19 @@ const url =
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+    name: String,
+    number: String
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (newName !== undefined & newNumber !== undefined) {
     const person = new Person({
-    name: newName,
-    number: newNumber
+        name: newName,
+        number: newNumber
     })
 
-    person.save().then(response => {
+    person.save().then(() => {
         console.log(`added ${newName} number ${newNumber} to phonebook`)
         mongoose.connection.close()
     })
@@ -35,7 +35,7 @@ if (newName !== undefined & newNumber !== undefined) {
     console.log('phonebook:')
     Person.find({}).then(result => {
         result.forEach(person => {
-          console.log(person.name + ' ' + person.number)
+            console.log(person.name + ' ' + person.number)
         })
         mongoose.connection.close()
     })

009136b3aa92c9447680155e5637038935c64e28
henskumari <hennaojala99@gmail.com>
2021-06-21 13:31:47 +0300

    tehtävät 3.19-3.21

--- models/person.js
+++ models/person.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 const url = process.env.MONGODB_URI
 
@@ -12,8 +13,15 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFind
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: 3,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8
+  }
 })
 
 personSchema.set('toJSON', {
